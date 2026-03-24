6768e661c8d8aefc1dce9fb7926c29f2417784b8
Thyon3 <asnakemengesha80@gmail.com>
2026-03-24 14:00:00 -0400

    Enhance Logo component with smooth theme transitions

--- components/Layout/Header/Logo.tsx
+++ components/Layout/Header/Logo.tsx
@@ -1,8 +1,9 @@
 import { LogoDark, LogoLight, LogoMobile } from '../../Icons/Icons';
 
+// Responsive Logo component with theme support
 const Logo = () => {
     return (
-        <div id="logo" className="bg-white py-5 px-4 dark:bg-[#2B2C37] sm:py-7 sm:px-6 lg:py-8 lg:px-8">
+        <div id="logo" className="bg-white py-5 px-4 dark:bg-[#2B2C37] sm:py-7 sm:px-6 lg:py-8 lg:px-8 transition-colors duration-200">
             <LogoLight className={`hidden dark:sm:block`} />
             <LogoDark className={`hidden sm:block dark:sm:hidden`} />
             <LogoMobile className={`sm:hidden`} />
