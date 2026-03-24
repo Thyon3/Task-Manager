15a03d60f749e1c56ab0e2385c31a9a1f1ffe7a3
Thyon3 <asnakemengesha80@gmail.com>
2026-03-24 14:00:00 -0400

    Enhance _app.tsx with improved documentation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import ThemeContextProvider from '../store/ThemeContext';
 import BoardListContextProvider from '../store/BoardListContext';
 import { SessionProvider } from 'next-auth/react';
 
+// Main App component with context providers
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     return (
         <SessionProvider session={session}>
