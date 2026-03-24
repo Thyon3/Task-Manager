9c0813e9921d858b27b12638c4cbe6374e25dff3
Thyon3 <asnakemengesha80@gmail.com>
2026-03-24 14:00:00 -0400

    Enhance Next.js config with improved documentation

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // Redirect /board to home page
     redirects: async () => {
         return [
             {
